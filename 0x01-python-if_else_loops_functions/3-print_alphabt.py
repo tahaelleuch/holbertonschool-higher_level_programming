a58e87be0cd60b1e2a2710f5107c3f89cc2b103a
Taha Elleuch <tahaeleuch@gmail.com>
2020-04-28 18:49:16 +0000

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
+for i in range(ord('a'), ord('z') + 1):
     if i != ord('e') and i != ord('q'):
-        print(chr(i), end='')
+        print("{:c}".format(i), end='')

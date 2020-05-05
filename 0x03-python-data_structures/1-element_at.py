f7833c64f21b5271794b4a84608261fdef8b9fea
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-05 14:27:46 +0000

    python

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+#!/use/bin/python3
+def element_at(my_list, idx):
+    if 0 <= idx < len(my_list):
+        return (my_list[idx])
+    else:
+        return (None)
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    new_string = ""
+    for i in my_string:
+        if i not in "cC":
+            new_string = new_string + i
+    return (new_string)

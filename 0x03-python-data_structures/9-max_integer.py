7b8b28fd2bfcda6e68563e3888fa87402cb33633
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-05 19:24:53 +0000

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
     a = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > a:
-            a = my_list[i] 
+            a = my_list[i]
     return (a)

35aed38648fee82ee75ffa7326bfd067b2a4a00d
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-18 20:15:53 +0000

    exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        r = a / b
+    except ZeroDivisionError:
+        r = None
+    finally:
+        print("Inside result: {}".format(r))
+    return (r)

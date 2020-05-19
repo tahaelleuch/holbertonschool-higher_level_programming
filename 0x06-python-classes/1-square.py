094ddff800ba03d603bd12ac930ac6d4ee6b9662
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-19 16:11:45 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""definition of class square"""
+
+
+class Square:
+    """Represntation of a square"""
+    def __init__(self, size):
+        """Initializes the square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size

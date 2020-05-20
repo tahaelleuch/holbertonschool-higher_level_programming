84b5a6b05705eb32ecbd4379762610be4401fca0
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-20 16:26:39 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Definition of a class square"""
+
+
+class Square:
+    """Represntation of a square"""
+    def __init__(self, size=0):
+        """Initializes the square
+        Args:
+            size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """Return the square area"""
+        return self.__size ** 2

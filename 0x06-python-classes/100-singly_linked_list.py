0b44154ae7052760fd4128b4af01a2636a5fed14
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-21 22:55:01 +0000

    100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+
+class Node:
+    """Representation of a node"""
+
+    def __init__(self, data, next_node=None):
+        """initializes the l list
+        Args:
+            data (int): data
+            next_node (Node): the next node
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """getter of data
+        Return:
+            data of the node
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """setter of data
+        Args:
+            value (int): data of the node
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """getter of next_node
+        Returns:
+           the next node
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """setter of next_node
+        Args:
+            value (Node): next node in the linked list
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Representation of a linked list"""
+    def __init__(self):
+        """Initialization of linked list
+        """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """insert new Node
+        Args:
+            value (int): data of the new node
+        """
+        new = Node(value)
+        tmp = self.__head
+        if tmp is None or tmp.data >= value:
+            if tmp:
+                new.next_node = tmp
+            self.__head = new
+            return
+        while tmp.next_node is not None:
+            if tmp.next_node.data >= value:
+                break
+            tmp = tmp.next_node
+        new.next_node = tmp.next_node
+        tmp.next_node = new

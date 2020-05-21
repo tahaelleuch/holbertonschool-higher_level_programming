992d52aa1c6cfe494db838b116a33e2d777f4add
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-21 03:58:04 +0000

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Definition of a class square"""
+
+
+class Square:
+    """Represntation of a square"""
+    def __init__(self, size=0):
+        """Initializes the square
+        Args:
+            size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return the square area
+
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Get __size
+        Returns:
+            size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+
+        Args:
+            size (int): size of size of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        """print a square
+
+        Return:
+            printed square
+        """
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+        else:
+            print()

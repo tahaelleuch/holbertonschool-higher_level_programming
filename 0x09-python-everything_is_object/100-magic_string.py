fb583f27ebfb25244306f544546282cd928531dc
Taha Elleuch <tahaeleuch@gmail.com>
2020-05-28 01:04:26 +0000

    0x09

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(list_str = []):
+    list_str.append("Holberton")
+    return ", ".join(list_str)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class for Loked class"""
+
+
+class LockedClass:
+    """LockedClass class"""
+    __slots__ = ['first_name']

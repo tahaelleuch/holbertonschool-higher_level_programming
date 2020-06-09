b692dfae88a9fd8542b7434eaf9f7b117cf4a3a2
Taha Elleuch <tahaeleuch@gmail.com>
2020-06-09 15:07:27 +0000

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """inisialization of the square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return str representation of square"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id,
+                self.x, self.y, self.width))

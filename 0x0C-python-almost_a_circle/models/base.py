62a729e6ba94aae62ecd377b5558f01b5ba942ae
Taha Elleuch <tahaeleuch@gmail.com>
2020-06-09 22:23:58 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,31 @@ class Base:
                 list_o.append(cls.to_dictionary(i))
         with open(f, 'w') as file:
             file.write(cls.to_json_string(list_o))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string"""
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ is "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ is "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        list_o = []
+        f = cls.__name__ + ".json"
+        with open(f, 'r') as file:
+            list_o = cls.from_json_string(file.read())
+        for i, e in enumerate(list_o):
+            list_o[i] = cls.create(**list_o[i])
+        return list_o

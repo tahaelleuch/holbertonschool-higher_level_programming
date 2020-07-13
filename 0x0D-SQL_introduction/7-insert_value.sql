4212b86f0dc575bc7bb66703eab0a20be943ddc9
Taha Elleuch <tahaeleuch@gmail.com>
2020-07-13 20:16:03 +0000

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts a new row in the table first_table
 
-INSERT INTO 'first_table'('id', 'name') VALUES (89, "Holberton School");
+INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Holberton School");

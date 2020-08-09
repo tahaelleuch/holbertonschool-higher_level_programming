08662c46e70f9995fb24d41c3f7c1f736260f39a
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-09 20:25:39 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" ists all states from the database hbtn_0e_0_usa"""
+"""script that takes in an argument and displays all values in the states
+ table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 from sys import argv

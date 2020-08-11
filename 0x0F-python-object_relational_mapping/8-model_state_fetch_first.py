b22dfe8e76d828f4051a46bd2276c5899afcc553
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-11 18:47:31 +0000

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,9 +10,10 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}\
-    @localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    @localhost:3306/{}".format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
     one = session.query(State).first()
     if one:

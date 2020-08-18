99a8641c9c3a502a90d7a47ab0bd80b47347f42e
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-18 17:12:52 +0000

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#URL passed as an argument, and displays only the status code of the response
-curl -o /dev-head --write-out '%{http_code}' "$1"
+# get status
+curl -o /dev/null --head --write-out '%{http_code}' "$1" -s

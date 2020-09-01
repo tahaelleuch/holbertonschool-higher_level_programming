d13a0b324cd80d3ad5ff4532fdda91b9271203ff
Taha elleuch <tahaeleuch@gmail.com>
2020-09-01 20:32:11 +0000

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+
+import requests
+
+
+if __name__ == '__main__':
+    req = requests.get('https://intranet.hbtn.io/status')
+    text = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))

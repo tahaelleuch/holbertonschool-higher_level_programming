147b13c9fcf37a29dafc7c640788926ece71f2e8
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-21 20:51:21 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,12 +8,12 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
-    } else {
+    if (c === undefined) {
       this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
     }
   }
 };

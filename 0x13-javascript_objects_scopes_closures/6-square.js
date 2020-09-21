a06434a4eac730a72c7ef5e8190c668b3103b792
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-21 20:54:58 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 /* create square class */
 module.exports = class Square extends require('./5-square.js') {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       this.print();

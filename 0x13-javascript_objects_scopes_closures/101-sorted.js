fd97a4cc65e2df93aeb2406289b9737a31ec5951
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-21 23:48:14 +0100

    102

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* sorted */
+const dict = require('./101').dict;
+const sorted = {};
+for (const key in dict) {
+  if (sorted[dict[key]] === undefined) {
+    sorted[dict[key]] = [];
+  }
+  sorted[dict[key]].push(key);
+}
+console.log(sorted);

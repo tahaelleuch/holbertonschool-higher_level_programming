56d073ee98ad9bae3b8c5489cb2d9f4372f384ea
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-21 19:57:03 +0100

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/* create rectangle class */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 & h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const a = this.width;
+    this.width = this.height;
+    this.height = a;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};

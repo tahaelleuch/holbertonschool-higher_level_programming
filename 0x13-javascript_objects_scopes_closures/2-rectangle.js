f0a7a65a8d78d880f0a2ea290f63c4ecde9006f2
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-21 19:41:56 +0100

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* create rectangle class */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 & h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};

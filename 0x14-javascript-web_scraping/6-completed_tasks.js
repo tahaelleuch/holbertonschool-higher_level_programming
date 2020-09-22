06c974f176b23235d7c18d1b16cbf3faf60cc852
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-22 19:50:47 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,15 +7,12 @@ request(process.argv[2], function (error, response, body) {
   }
   const Mybody = JSON.parse(body);
   const dict = {};
-  for (const i in Mybody) {
-    dict[Mybody[i].userId] = 0;
-  }
   for (const j in Mybody) {
     if (Mybody[j].completed === true) {
       if (dict[Mybody[j].userId] === undefined) {
         dict[Mybody[j].userId] = 1;
       } else {
-      dict[Mybody[j].userId]++;
+        dict[Mybody[j].userId]++;
       }
     }
   }

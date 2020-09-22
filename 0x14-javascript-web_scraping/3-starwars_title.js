f2ce59c0304d01209b5460ccf2ad2a618df25239
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-22 13:24:07 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,8 @@
 const request = require('request');
 const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 request(link, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
   console.log(JSON.parse(body).title);
 });

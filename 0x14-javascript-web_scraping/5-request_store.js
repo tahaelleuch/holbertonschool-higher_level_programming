43bba580957bb75db8725430992f5e26b4638123
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-22 14:20:29 +0100

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  const fs = require('fs');
+  fs.writeFile(process.argv[3], body, 'utf-8', function (err, text) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
